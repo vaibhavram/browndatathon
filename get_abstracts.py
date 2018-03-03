5f129359db839fee86f7846cb8a6afde2365c9c6
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2018-03-03 16:39:43 -0500

    getting year and url too

--- get_abstracts.py
+++ get_abstracts.py
@@ -12,6 +12,8 @@ dois = de["manuscript_DOI"].unique()
 
 # authors = pd.Series(name = "authors")
 abstracts = pd.Series(name = "abstract")
+urls = pd.Series(name = "url")
+years = pd.Series(name = "year")
 
 for i in range(len(dois)):
 	d = re.sub("/", "%2F", dois[i])
@@ -20,6 +22,15 @@ for i in range(len(dois)):
 	page = urllib.request.urlopen(url)
 	soup = BeautifulSoup(page, "html.parser")
 	soup_split = soup.text.split("\n\n")
+
+	year = 0
+	line1 = re.sub("\n", "", soup_split[1]).split(" ")
+	for word in line1:
+		if len(word) == 4 and word.isdigit():
+			print(word)
+			year = int(word)
+			break
+	years = years.append(pd.Series([year]))
 
 	count = 0
 	for i in soup_split:
@@ -31,14 +42,15 @@ for i in range(len(dois)):
 		abstract = soup_split[5]
 		if abstract.split(" ")[0] == "Comment" or abstract.split(" ")[0] == "Erratum":
 			abstract = soup_split[6]
-			if abstract.split(" ")[0] == "Update":
+			if abstract.split(" ")[0] == "Update" or abstract.split(" ")[0] == "Comment":
 				abstract = soup_split[7]
 		if abstract.split(" ")[0] == "DOI:":
 			abstract = soup_split[4]
 	abstract = re.sub("\n", " ", abstract)
 	# authors = authors.append(pd.Series([article.authors]))
 	abstracts = abstracts.append(pd.Series([re.sub(",", "", abstract)]))
+	urls = urls.append(pd.Series([url]))
 
-abstracts_table = pd.DataFrame(data = { "doi" : dois, "abstract" : abstracts })
+abstracts_table = pd.DataFrame(data = { "doi" : dois, "abstract" : abstracts, "url" : url, "year" : years })
 
 abstracts_table.to_csv("abstracts.csv")
