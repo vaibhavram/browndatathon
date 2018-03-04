d35fe96ca8e3a7a0bc35ca21eb313f993c912646
Jong Lee <jonghalee@berkeley.edu>
2018-03-04 03:17:27 -0500

    done

--- TextVisual.R
+++ TextVisual.R
@@ -2,7 +2,7 @@
 library(tm)
 library(reshape2)
 library(dplyr)
-library(scatterplot3d)
+library(plotly)
 setwd("~/Desktop/datathon/brown/browndatathon")
 df <- read.csv("together_data.csv", stringsAsFactors = F)
 
@@ -28,7 +28,7 @@ scores <- inner_join(scores, unique(df[ ,c("abstract", "manuscript_id")]),
                      by = "manuscript_id")
 # Creating presence Corpus
 presenceCorp <- data.frame()
-
+numAppear <- c()
 for (i in 1:length(uniqWords)) {
   if (i %% 100 == 0) {
     cat("Finished", i, "th word presence finding..\n")
@@ -40,16 +40,46 @@ for (i in 1:length(uniqWords)) {
   avgScores <- apply(scores[presence, c("avgDV", "avgAct", "avgCC")], 
                      2, function(x) mean(x[1]))
   presenceCorp <- rbind(presenceCorp, avgScores)
+  numAppear <- c(numAppear, sum(presence))
 }
 presenceCorp['word'] <- uniqWords
-colnames(presenceCorp) <- c('avgDV', 'avgAct', 'avgCC', 'word')
+presenceCorp['numAppear'] <- numAppear
+colnames(presenceCorp) <- c('avgDV', 'avgAct', 'avgCC', 'word', 'numAppear')
 
 presenceCorp <- presenceCorp[!is.na(presenceCorp$avgAct), ]
+
+presenceCorp[,1:3] <- 
+  apply(presenceCorp[,1:3], 2, function(x) {
+  round(x, 2)
+})
 
 saveRDS(presenceCorp, file = "wordScoreMat.rds")
 
 presenceCorp <- readRDS(file = "wordScoreMat.rds")
-x <- presenceCorp[,1]
-y <- presenceCorp[,2]
-z <- presenceCorp[,3]
-ptext3d(presenceCorp[,1:3], text = presenceCorp$word)
+
+distMat <- 
+  apply(presenceCorp[,1:3], 2, function(x) {
+  (x - mean(x)) / sd(x)
+})
+distMat <- data.frame(distMat)
+distMat['word'] <- presenceCorp['word']
+distMat['numAppear'] <- presenceCorp['numAppear']
+
+sigMat <- distMat[ (abs(distMat[,1]) >= 2) | (abs(distMat[,2]) >= 2) | abs(distMat[,3] >= 2), ]
+
+# with(sigMat,plot3d(x,y,z))
+# with(presenceCorp,text3d(x,y,z,presenceCorp$word, font = 1))
+
+
+plot_ly(presenceCorp, x = ~avgDV, y = ~avgAct, z = ~avgCC,
+        text = ~paste0("(",avgDV, ",", avgAct, ",", avgCC, ")",
+                       "<br>Word: ", word, 
+                       "<br>num:", numAppear), 
+        hoverinfo = "text", color = ~log(numAppear),
+        marker = list(size =6)) %>%
+  layout(title = "Average Ratings for each Word, by Category (axis)",
+         scene = list(xaxis = list(title = 'Discovery'),
+                      yaxis = list(title = 'Actionable'),
+                      zaxis = list(title = 'Concrete')))
+
+
