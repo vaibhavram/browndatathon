556eb4a3efa0a4bb26c52b25f8c15944c1f234d6
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2018-03-04 00:44:56 -0500

    fixed everything

--- spread_table.R
+++ spread_table.R
@@ -12,16 +12,21 @@ data$question_id[data$question_id == 4] <- "expertise"
 data$create_date <- as.POSIXct(data$create_date, format = "%m/%d/%Y %H:%M", tz = "GMT")
 
 data2 <- data %>%
-  group_by(question_id, version_id, user_id, manuscript_id, manuscript_DOI) %>%
+  group_by(question_id, user_id, manuscript_id, manuscript_DOI) %>%
   summarize(max(create_date))
 names(data2)[ncol(data2)] <- "create_date"
 data2 <- data2 %>%
   left_join(data, by = c("question_id" = "question_id", "create_date" = "create_date", 
-                         "version_id" = "version_id", "user_id" = "user_id", 
-                         "manuscript_id" = "manuscript_id", "manuscript_DOI" = "manuscript_DOI")) %>%
+                         "user_id" = "user_id", "manuscript_id" = "manuscript_id", 
+                         "manuscript_DOI" = "manuscript_DOI")) %>%
   arrange(manuscript_id, user_id, question_id)
 
-new_data <- dcast(data, create_date + version_id + user_id + manuscript_id + manuscript_DOI ~ question_id,
+data3 <- data2 %>% ungroup() %>%
+  group_by(question_id, user_id, manuscript_id, manuscript_DOI, version_id, create_date) %>%
+  summarize(rating = max(rating))
+
+new_data <- dcast(data3, create_date + version_id + user_id + manuscript_id + manuscript_DOI ~ question_id,
                   value.var = "rating", fun.aggregate = mean, na.rm = TRUE)
 
 write.csv(new_data, "ratings.csv")
+
