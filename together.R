88ef716167cd002fe805c9d47129260af09e3fae
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2018-03-03 22:39:58 -0500

    pushing eveetyhting

--- together.R
+++ together.R
@@ -10,7 +10,8 @@ abstracts <- abstracts[,2:ncol(abstracts)]
 ratings <- ratings[,2:ncol(ratings)]
 
 data <- ratings %>%
-  left_join(abstracts, by = c("manuscript_DOI" = "doi"))
+  left_join(abstracts, by = c("manuscript_DOI" = "doi")) %>%
+  filter(year > 0)
 
 data$abstract_nchar <- nchar(data$abstract)
 data$abstract_num_digits <- str_count(data$abstract, "0") + str_count(data$abstract, "1") +
@@ -21,11 +22,39 @@ data$abstract_has_digits <- data$abstract_num_digits > 0
 data$abstract_findings_results <- str_count(data$abstract, "result") + 
   str_count(data$abstract, "finding") + str_count(data$abstract, "Result") + 
   str_count(data$abstract, "Finding")
+data$dv_words <- str_count(data$abstract, "transform") + 
+  str_count(data$abstract, "importan") + str_count(data$abstract, "discover") + 
+  str_count(data$abstract, "new") + str_count(data$abstract, "innovat") + 
+  str_count(data$abstract, "chang") + str_count(data$abstract, "potential") + 
+  str_count(data$abstract, "breakthrough")
+data$act_words <- str_count(data$abstract, "use") + 
+  str_count(data$abstract, "relevan") + str_count(data$abstract, "applicab") + 
+  str_count(data$abstract, "utility") + str_count(data$abstract, "utiliz") + 
+  str_count(data$abstract, "suit") + str_count(data$abstract, "action")
+data$con_words <- str_count(data$abstract, "accura") + 
+  str_count(data$abstract, "confirm") + str_count(data$abstract, "ground") + 
+  str_count(data$abstract, "confiden") + str_count(data$abstract, "concrete") + 
+  str_count(data$abstract, "truth")
 
 data$version_id <- as.factor(data$version_id)  
 data$year <- as.factor(data$year)
 
-lm1 <- lm(discovery_value ~ abstract_nchar + abstract_num_digits + 
-            abstract_has_digits + abstract_findings_results + version_id + year + expertise, data)  
-  
-  
+lm_dv <- lm(discovery_value ~ abstract_nchar + abstract_num_digits + 
+            abstract_has_digits + abstract_findings_results + version_id + year + expertise +
+            dv_words + act_words + con_words, data)  
+lm_act <- lm(actionability ~ abstract_nchar + abstract_num_digits + 
+              abstract_has_digits + abstract_findings_results + version_id + year + expertise +
+              dv_words + act_words + con_words, data)  
+lm_con <- lm(concreteness_confidence ~ abstract_nchar + abstract_num_digits + 
+              abstract_has_digits + abstract_findings_results + version_id + year + expertise +
+              dv_words + act_words + con_words, data)  
+summary(lm_dv)
+summary(lm_act)
+summary(lm_con)
+
+
+######
+View(data %>%
+  group_by(user_id) %>%
+  summarize(count = n()) %>%
+  arrange(desc(count)))
